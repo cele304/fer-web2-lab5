391d199716fb8f6b6e556376241fd2be508d7b7e
cele304 <129319909+cele304@users.noreply.github.com>
2024-12-28 20:24:45 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    port: parseInt(process.env.PORT) || 5173, // Koristi port iz varijable okruženja ili fallback na 5173
+    host: true, // Omogućuje aplikaciji da sluša na svim mrežnim sučeljima
+  },
 });
